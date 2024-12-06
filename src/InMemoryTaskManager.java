c0764823c67b7eedc105eb4db98b3778128f7b99
Fortuna.S <psypsy77@yandex.ru>
2024-12-06 22:07:18 +0300

    Вторая версия ТЗ-5

--- src/InMemoryTaskManager.java
+++ src/InMemoryTaskManager.java
@@ -7,7 +7,7 @@ public class InMemoryTaskManager implements TaskManager {
     private final HashMap<Integer, Epic> epics = new HashMap<>();
     private final HashMap<Integer, Subtask> subtasks = new HashMap<>();
 
-    public HistoryManager historyManager = Managers.getDefaultHistory();
+    private final HistoryManager historyManager = Managers.getDefaultHistory();
 
     private int id = 0;
 
